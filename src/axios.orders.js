aae7823c3c1d24738e36a1c1bf7fd0c21a5092e6
piyushabbi <piyushabbi27@gmail.com>
2018-06-09 00:23:10 +0530

    get initial ingredients

--- src/axios.orders.js
+++ src/axios.orders.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 // Create an axios instance
 const instance = axios.create({
-	baseURL: 'https://react-my-food.firebaseio.com/'
+	baseURL: 'https://react-my-food.firebaseio.com/',
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json'
+	}
 });
 
 export default instance;
