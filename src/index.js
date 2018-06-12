b7096a72d68a6f899788237faae06e747b8badbe
piyushabbi <piyushabbi27@gmail.com>
2018-06-12 20:11:17 +0530

    Setup Redux Devtools

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import App from './App';
 import reducer from './reducers/reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
 	<Provider store={store}>
